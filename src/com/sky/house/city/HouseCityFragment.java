4368bd4a9904bff0421488c20977d445f3dcae4b
skypan <skypan@eroadsoftware.com>
2015-05-12 17:43:54 +0800

    no message

--- src/com/sky/house/city/HouseCityFragment.java
+++ src/com/sky/house/city/HouseCityFragment.java
@@ -4,21 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.eroad.base.BaseFragment;
+import com.eroad.base.util.ConfigDefinition;
+import com.next.intf.ITaskListener;
+import com.next.net.SHPostTaskM;
+import com.next.net.SHTask;
 import com.sky.house.R;
+import com.sky.widget.SHDialog;
+import com.sky.widget.sweetdialog.SweetDialog;
 /**
  * 选择城市
  * @author skypan
  *
  */
-public class HouseCityFragment extends BaseFragment {
+public class HouseCityFragment extends BaseFragment implements ITaskListener{
 
+	private SHPostTaskM taskCity;
+	
+	private ListView mLvCity,mLvLetter;
+	
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onViewCreated(view, savedInstanceState);
 		mDetailTitlebar.setTitle("城市");
+		requestCity();
 	}
 
 	@Override
@@ -28,4 +40,36 @@ public class HouseCityFragment extends BaseFragment {
 		return view;
 	}
 
+	private void requestCity(){
+		SHDialog.ShowProgressDiaolg(getActivity(), null);
+		taskCity = new SHPostTaskM();
+		taskCity.setUrl(ConfigDefinition.URL+"getAllCity");
+		taskCity.setListener(this);
+		taskCity.start();
+	}
+
+	@Override
+	public void onTaskFinished(SHTask task) throws Exception {
+		// TODO Auto-generated method stub
+		SHDialog.dismissProgressDiaolg();
+	}
+
+	@Override
+	public void onTaskFailed(SHTask task) {
+		// TODO Auto-generated method stub
+		SHDialog.dismissProgressDiaolg();
+		new SweetDialog(getActivity(), SweetDialog.ERROR_TYPE).setTitleText("提示").setContentText(task.getRespInfo().getMessage()).show();
+	}
+
+	@Override
+	public void onTaskUpdateProgress(SHTask task, int count, int total) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTaskTry(SHTask task) {
+		// TODO Auto-generated method stub
+		
+	}
 }
